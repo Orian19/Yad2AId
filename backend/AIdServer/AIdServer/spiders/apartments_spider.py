1223d443b8fce95d9bd21ce5479d3de779751fd2
Orian19 <orian19@gmail.com>
2024-06-03 13:36:36 +0300

    [#2] add pagination

--- backend/AIdServer/AIdServer/spiders/apartments_spider.py
+++ backend/AIdServer/AIdServer/spiders/apartments_spider.py
@@ -12,6 +12,7 @@ class ApartmentsSpider(scrapy.Spider):
     start_urls = {
         r'https://www.yad2.co.il/realestate/rent'
     }
+    page_number = 2  # pagination
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         # open_in_browser(response)  # for debugging purposes
@@ -21,11 +22,15 @@ class ApartmentsSpider(scrapy.Spider):
         title = response.css("title::text").extract()
         items['title'] = title
 
+        # user = class ="user-drop-container_profileBoxText__deZ0a user-drop-container_wideDesktop__wuusc" > Orian < /span >
+
         # sends item to pipline
         yield items
 
         # next_page = response.css('<a data-testid="nextButton" class="link_link__vOomn" aria-label="עמוד הבא" aria-disabled="false" data-nagish="pagination-item-link" href="/realestate/rent?page=2"><svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="link_icon__qM0QO"><path d="M15.014 17.884a.75.75 0 01-.98 1.13l-.084-.073-6.137-6.18a.75.75 0 01-.069-.977l.073-.083 6.145-6.103a.75.75 0 011.13.98l-.073.085-5.613 5.573 5.608 5.648z" fill="currentColor"></path></svg></a>')
+        next_page = f'https://www.yad2.co.il/realestate/rent?page={str(ApartmentsSpider.page_number)}'
 
         # # todo: decide if to scrape all 1000+ pages or limit
-        # if next_page:
-        #     yield response.follow(next_page, callback=self.parse)
+        if ApartmentsSpider.page_number <= 3:
+            ApartmentsSpider.page_number += 1
+            yield response.follow(next_page, callback=self.parse)
