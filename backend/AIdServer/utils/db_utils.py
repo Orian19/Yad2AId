0558bd00b97392f94e09fb92bc00b25da95dbb2d
Orian19 <orian19@gmail.com>
2024-06-03 18:00:42 +0300

    [#2] add utils package with database utils

---
+++ backend/AIdServer/utils/db_utils.py
@@ -0,0 +1,26 @@
+import sqlite3
+import numpy as np
+import io
+
+
+def adapt_array(arr):
+    """
+    adapt numpy array to binary format for SQLite
+    :param arr: numpy array
+    :return:
+    """
+    out = io.BytesIO()
+    np.save(out, arr)
+    out.seek(0)
+    return sqlite3.Binary(out.read())
+
+
+def convert_array(text):
+    """
+    convert binary format back to numpy array.
+    :param text:
+    :return: numpy array
+    """
+    out = io.BytesIO(text)
+    out.seek(0)
+    return np.load(out, allow_pickle=True)
