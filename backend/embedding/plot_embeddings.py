e4b1659374f104d0288a6389bf66dd50fa51226e
dana.stok <dana.stok@post.runi.ac.il>
2024-06-10 19:38:27 +0300

    Seperated plot embeddings and find similar apartments files

--- backend/embedding/plot_embeddings.py
+++ backend/embedding/plot_embeddings.py
@@ -97,38 +97,7 @@ def plot_embeddings(ids_embeddings, cities_to_include):
     plt.show()
 
 
-def find_nearest_neighbors(ids_embeddings):
-    """Find the nearest neighbor for each apartment's pre-fetched embedding."""
-    apartment_id, city_name, embedding = zip(*ids_embeddings)
-    embeddings = np.array(embedding)
-
-    # Compute cosine similarity matrix (since embeddings are normalized, this can be done using dot product)
-    similarity_matrix = np.dot(embeddings, embeddings.T)
-    np.fill_diagonal(similarity_matrix, -np.inf)  # to ignore self-similarity
-
-    # Find the index of the maximum similarity for each embedding (excluding self)
-    nearest_indices = np.argmax(similarity_matrix, axis=1)
-    nearest_ids = [apartment_id[idx] for idx in nearest_indices]
-
-    return dict(zip(apartment_id, nearest_ids))
-
-def display_results_in_popup(nearest_neighbors):
-    """Displays nearest neighbors in a pop-up window using tkinter."""
-    window = tk.Tk()
-    window.title("Nearest Neighbors Display")
-    window.geometry('400x300')  # Set the size of the window
-
-    txt = scrolledtext.ScrolledText(window, wrap=tk.WORD)
-    txt.pack(pady=20, padx=20, fill=tk.BOTH, expand=True)
-    for apartment_id, nearest_neighbor_id in nearest_neighbors.items():
-        txt.insert(tk.END, f"Apartment ID {apartment_id} is most similar to Apartment ID {nearest_neighbor_id}\n")
-
-    # Start the GUI event loop
-    window.mainloop()
-
 # Example usage
 ids_embeddings = fetch_ids_and_embeddings()
-#nearest_neighbors = find_nearest_neighbors(ids_embeddings)
-#display_results_in_popup(nearest_neighbors)
 cities_to_plot = ["Ashkelon", "Tel Aviv Jaffa", "Haifa", "Bat Yam", "Ramat Gan", "Beer Sheva", "Netanya", "Jerusalem", "Raanana", "Kfar Saba"]
-plot_embeddings(ids_embeddings, cities_to_plot)
+#plot_embeddings(ids_embeddings, cities_to_plot)
