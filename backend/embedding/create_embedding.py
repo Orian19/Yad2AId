1284e690a91a0d6f766b9ae948955e21fb245643
dana.stok <dana.stok@post.runi.ac.il>
2024-06-14 13:28:41 +0300

    changed translation

--- backend/embedding/create_embedding.py
+++ backend/embedding/create_embedding.py
@@ -1,4 +1,3 @@
-from googletrans import Translator
 import openai
 from dotenv import load_dotenv
 import numpy as np
@@ -7,17 +6,6 @@ import os
 # load API key
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-def translate_to_english(text: str) -> str:
-    if not text:
-        return ""  # Return empty string if input is empty
-
-    translator = Translator()
-    # Automatically detect the source language and translate to English
-    translation = translator.translate(text, dest='en')
-    # Return only the translated text
-    return translation.text
-
 
 def normalize_l2(x):
     """Normalize an array to L2 norm."""
