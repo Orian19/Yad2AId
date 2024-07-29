8244f1e0081ca124d9c45604b2809b3cb21b9fc8
dana.stok <dana.stok@post.runi.ac.il>
2024-07-29 13:27:06 +0300

    [#4] Restructuring

--- content-script-loader.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const script = document.createElement('script');
-    script.src = chrome.runtime.getURL('content.js');
-    script.type = 'module';
-    (document.head || document.documentElement).appendChild(script);
-})();
