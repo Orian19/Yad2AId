3b676f799d68955c00734faff28c081b6037aaa8
Orian19 <orian19@gmail.com>
2024-07-31 20:24:20 +0300

    [#6] change function schedule to 10min for testing

--- backend/function_app.py
+++ backend/function_app.py
@@ -4,7 +4,8 @@ from utils.refresh_apts_urls import refresh_apts_urls
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+# @app.schedule(schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+@app.schedule(schedule="0 */10 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
