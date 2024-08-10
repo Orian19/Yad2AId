7ada4f39efaf51cd3a2e2b73e6625278c42a8899
Orian19 <orian19@gmai.com>
2024-08-10 23:04:20 +0300

    [#6] fixes for azure deployment
    - db path
    - requirements
    - startup.sh file

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,1 +1,14 @@
-python -m uvicorn fastApiApp.apartment_finder:app --host 0.0.0.0
+#!/bin/bash
+
+# Activate the virtual environment
+# source /home/site/wwwroot/venv/bin/activate
+
+# Start your FastAPI app
+python -m uvicorn fastApiApp.apartment_finder:app --host 0.0.0.0 --port 8000
+
+# Set up your scheduled tasks
+# python -m functions.function1 &
+# python -m functions.function2 &
+
+# Keep the container running
+tail -f /dev/null
