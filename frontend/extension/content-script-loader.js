c56d77fcc352a8ee248f86e0070a1ec52ef884f2
dana.stok <dana.stok@post.runi.ac.il>
2024-08-14 18:21:49 +0300

    [#7] Analytics working - Login analytics implemented

--- frontend/extension/content-script-loader.js
+++ frontend/extension/content-script-loader.js
@@ -1,6 +1,18 @@
 (function() {
-    const script = document.createElement('script');
-    script.src = chrome.runtime.getURL('frontend/extension/user/content.js');
-    script.type = 'module';
-    (document.head || document.documentElement).appendChild(script);
+    // Load the analytics script first
+    const analyticsScript = document.createElement('script');
+    analyticsScript.src = chrome.runtime.getURL('frontend/extension/analytics/analytics.js');
+    analyticsScript.type = 'text/javascript';
+    analyticsScript.onload = function() {
+        console.log("Analytics script loaded");
+    };
+    (document.head || document.documentElement).appendChild(analyticsScript);
+
+    // Load the content script after analytics is loaded
+    analyticsScript.onload = function() {
+        const contentScript = document.createElement('script');
+        contentScript.src = chrome.runtime.getURL('frontend/extension/user/content.js');
+        contentScript.type = 'module';
+        (document.head || document.documentElement).appendChild(contentScript);
+    };
 })();
