9c78671f072fbd98a01989d7bc4ba37ba07f7d97
dana.stok <dana.stok@post.runi.ac.il>
2024-08-17 17:09:03 +0300

    [#8] Added path to spiders

--- backend/scheduler/scheduler.py
+++ backend/scheduler/scheduler.py
@@ -1,9 +1,14 @@
 import subprocess
+import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 from backend.utils.db_utils import create_connection
 from backend.embedding.update_english_columns import update_english_description_column
 from backend.embedding.insert_embedding import insert_embeddings
 from backend.utils.refresh_apts_urls import refresh_apts_urls
+
+# Define the path to the Scrapy project
+SCRAPY_PROJECT_PATH = os.path.join(os.path.dirname(__file__), '..', 'AIdServer', 'AIdServer')
+
 
 # Your function that updates the database
 def update_database():
@@ -13,10 +18,10 @@ def update_database():
     refresh_apts_urls()
         
     # 1. Run apartment crawler to get new apartments
-    subprocess.run(["scrapy", "crawl", "apartments"])
+    subprocess.run(["scrapy", "crawl", "apartments"], cwd=SCRAPY_PROJECT_PATH)
     
     # 2. Run description crawler to get descriptions of apartments
-    subprocess.run(["scrapy", "crawl", "descriptions"])
+    subprocess.run(["scrapy", "crawl", "descriptions"], cwd=SCRAPY_PROJECT_PATH)
     
     # create connection to database
     con, cur = create_connection()
